efac2688608a3a14f06de3c6c4a8b1f973935805
Grant Derepas <grant@g-force.net>
2025-03-05 18:21:07 +0800

    Remove Cypress Config

--- src/animations/bounce.ts
+++ src/animations/bounce.ts
@@ -2,6 +2,7 @@ import Variables from '../config.ts';
 import { GlobalVars, GlobalConst } from '../types';
 const { globalVars, globalConst} = Variables;
 import helpers from '../helpers.ts';
+import { gsap } from "gsap";
 
 export function emoteBounce(images: string[], count: number = 5, interval: number = 100): void {
     let imgcount = images.length;
